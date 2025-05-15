1fcd6654656cff7e10570c589fa5a421f2322f63
Martin Taylor <martin@Martins-MacBook-Pro-2.local>
2025-05-15 17:32:23 +0100

    changed sketchybar layout

--- config/sketchybar/items/spaces.sh
+++ config/sketchybar/items/spaces.sh
@@ -1,44 +1,50 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+sketchybar --add event aerospace_workspace_change
 
-sketchybar --add item spacer.1 left \
-  --set spacer.1 background.drawing=off \
-  label.drawing=off \
-  icon.drawing=off \
-  width=10
+sketchybar --add item aerospace_dummy left \
+  --set aerospace_dummy display=0 \
+  script="$PLUGIN_DIR/spaces.sh" \
+  --subscribe aerospace_dummy aerospace_workspace_change
 
-for i in {0..9}; do
-  sid=$((i + 1))
-  sketchybar --add space space.$sid left \
-    --set space.$sid associated_space=$sid \
-    label.drawing=off \
-    icon.padding_left=10 \
-    icon.padding_right=10 \
-    background.padding_left=-5 \
-    background.padding_right=-5 \
-    script="$PLUGIN_DIR/space.sh"
+for m in $(aerospace list-monitors | awk '{print $1}'); do
+  for sid in $(aerospace list-workspaces --monitor $m); do
+    sketchybar --add space space.$sid left \
+      --set space.$sid space=$sid \
+      icon=$sid \
+      background.color=$TRANSPARENT \
+      label.color=$ACCENT_COLOR \
+      icon.color=$ACCENT_COLOR \
+      display=$m \
+      label.font="sketchybar-app-font:Regular:12.0" \
+      icon.font="SF Pro:Semibold:12.0" \
+      label.padding_right=10 \
+      label.y_offset=-1 \
+      click_script="$PLUGIN_DIR/space_click.sh $sid"
+
+    apps=$(aerospace list-windows --monitor "$m" --workspace "$sid" |
+      awk -F '|' '{gsub(/^ *| *$/, "", $2); if (!seen[$2]++) print $2}')
+
+    icon_strip=""
+    if [ "${apps}" != "" ]; then
+      while read -r app; do
+        icon_strip+=" $($PLUGIN_DIR/icons.sh "$app")"
+      done <<<"${apps}"
+    else
+      icon_strip=" —"
+    fi
+
+    sketchybar --set space.$sid label="$icon_strip"
+
+  done
+  
+  for empty_space in $(aerospace list-workspaces --monitor $m --empty); do
+    sketchybar --set space.$empty_space display=0
+  done
+  for focus in $(aerospace list-workspaces --focused); do
+    sketchybar --set space.$focus background.drawing=on \
+      background.color=$ACCENT_COLOR \
+      label.color=$ITEM_COLOR \
+      icon.color=$ITEM_COLOR
+  done
 done
-
-sketchybar --add item spacer.2 left \
-  --set spacer.2 background.drawing=off \
-  label.drawing=off \
-  icon.drawing=off \
-  width=5
-
-sketchybar --add bracket spaces '/space.*/' \
-  --set spaces background.border_width="$BORDER_WIDTH" \
-  background.border_color="$RED" \
-  background.corner_radius="$CORNER_RADIUS" \
-  background.color="$BAR_COLOR" \
-  background.height=26 \
-  background.drawing=on
-
-sketchybar --add item separator left \
-  --set separator icon= \
-  icon.font="$FONT:Regular:16.0" \
-  background.padding_left=26 \
-  background.padding_right=15 \
-  label.drawing=off \
-  associated_display=active \
-  icon.color="$YELLOW"
--- config/sketchybar/plugins/cpu.sh
+++ config/sketchybar/plugins/cpu.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sketchybar --set "$NAME" icon="" label="$(ps -A -o %cpu | awk '{s+=$1} END {s /= 8} END {printf "%.1f%%\n", s}')"
+CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
+CPU_INFO=$(ps -eo pcpu,user)
+CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+
+CPU_PERCENT=$(ps -eo pcpu | awk -v core_count=$(sysctl -n machdep.cpu.thread_count) '{sum+=$1} END {printf "%.0f\n", sum/core_count}')
+sketchybar --set $NAME label="$CPU_PERCENT%"
